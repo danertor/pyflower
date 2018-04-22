889dee4bc499453b921ec29ff72f3d238c56d570
danertor <danertor@gmail.com>
2018-04-22 15:19:06 -0400

    Case study Performance multi proc/thrd

--- chapter13/case_study/compress_block.py
+++ chapter13/case_study/compress_block.py
@@ -17,10 +17,22 @@ def compress_row(row):
     compressed = bytearray()
     chunks = split_bits(row, 127)
     for chunk in chunks:
-        compressed.extend(compress_chunk())
+        compressed.extend(compress_chunk(chunk))
     return compressed
 
 # a generator to slip a row in width chunks
 def split_bits(bits, width):
     for i in range(0, len(bits), width):
         yield bits[i:i+width]
+
+
+def compress_in_executor(executor, bits, width):
+    row_compressors = []
+    for row in split_bits(bits, width):
+        compressor = executor.submit(compress_row, row)
+        row_compressors.append(compressor)
+
+    compressed = bytearray()
+    for compressor in row_compressors:
+        compressed.extend(compressor.result())
+    return compressed
